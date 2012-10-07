e1d68ecd808362a6bbae783c9b0ad99f04ee536a
Igor Mats <igor.mats@yandex.ru>
2012-10-07 16:05:33 +0300

    Null

--- src/com/tooflya/airbubblegum/entities/Chiky.java
+++ src/com/tooflya/airbubblegum/entities/Chiky.java
@@ -121,10 +121,12 @@ public class Chiky extends Entity {
 
 			this.timeToFall--;
 			if (this.timeToFall <= 0) {
-				Bubble airgum = (Bubble) Game.world.airgums.create();
-				airgum.setCenterPosition(this.getCenterX() + Game.random.nextInt(50) - 25, this.getCenterY() + Game.random.nextInt(50) - 25); // TODO: Correct.
-				airgum.setScale(this.airgumScale);
-				airgum.setIsScale(false);
+				if (this.airgumScale > Bubble.minScale) {
+					Bubble airgum = (Bubble) Game.world.airgums.create();
+					airgum.setCenterPosition(this.getCenterX() + Game.random.nextInt(50) - 25, this.getCenterY() + Game.random.nextInt(50) - 25); // TODO: Correct.
+					airgum.setScale(this.airgumScale);
+					airgum.setIsScale(false);
+				}
 				final int particlesCount = 7; // TODO: Correct later. Maybe need to make another function.
 				Particle particle;
 				for (int i = 0; i < particlesCount; i++) {
