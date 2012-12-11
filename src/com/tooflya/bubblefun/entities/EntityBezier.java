5d0fe4ab3ddeb604d40eab107832d8c413a5eee7
Ring-r <Ring_r@mail.ru>
2012-12-11 12:26:39 +0200

    Add init methods to EntityBezier. Fix #1.

--- src/com/tooflya/bubblefun/entities/EntityBezier.java
+++ src/com/tooflya/bubblefun/entities/EntityBezier.java
@@ -22,21 +22,19 @@ public class EntityBezier extends Entity {
 	// Fields
 	// ===========================================================
 
-	private float mX_ = 0; // Last (or old) x.
+	private float mTime = 0f; // Seconds.
 
-	protected float mTime = 0f; // Seconds.
-
-	public float mMinTime = 0; // Seconds.
-	public float mMaxTime = 1; // Seconds.
-	public float mSpeedTime = 0; // Points per seconds.
-	public float mOffsetTime = 0; // Seconds.
-	public boolean mIsReverseTime = true;
+	private float mMinTime = 0; // Seconds.
+	private float mMaxTime = 1; // Seconds.
+	private float mSpeedTime = 0; // Points per seconds.
+	private float mOffsetTime = 0; // Seconds.
+	private boolean mIsReverseTime = true;
 
 	public static float mKoefSpeedTime = 1;
 
 
-	public final byte mListBX[] = new byte[mListCapacity];
-	public final byte mListBY[] = new byte[mListCapacity];
+	private final byte mListBX[] = new byte[mListCapacity];
+	private final byte mListBY[] = new byte[mListCapacity];
 	private byte mListCount = 0;
 	
 	// ===========================================================
@@ -54,25 +52,64 @@ public class EntityBezier extends Entity {
 	// Methods
 	// ===========================================================
 
-	private void updatePoints()	{
-		for(int i = 0; i< this.mListCount; i++)	{
-			mListX[i] = this.mListBX[i];
-			mListY[i] = this.mListBY[i];
-		}
-		for (int j = 1; j < this.mListCount; j++) {
-			int count = this.mListCount - j;
-			for (int i = 0; i < count; i++) {
-				mListX[i] = mListX[i] + (mListX[i + 1] - mListX[i]) * this.mTime;
-				mListY[i] = mListY[i] + (mListY[i + 1] - mListY[i]) * this.mTime;
-			}
-		}
-		this.mX_ = this.mX;
-		this.setCenterPosition(mListX[0] / 100 * Options.screenWidth, mListY[0] / 100 * Options.screenHeight); // TODO: Correct using size and touchHeight.
+	public void init() {
+		this.mTime = 0f; // Seconds.
+		this.mMinTime = 0; // Seconds.
+		this.mMaxTime = 1; // Seconds.
+		this.mSpeedTime = 0; // Points per seconds.
+		this.mOffsetTime = 0; // Seconds.
+		this.mIsReverseTime = true;
+		this.mListCount = 0;
 	}
-
+	
+	public void initMinTime(final float pMinTime) {
+		this.mMinTime = pMinTime;
+	}
+	
+	public void initMaxTime(final float pMaxTime) {
+		this.mMaxTime = pMaxTime;
+	}
+	
+	public void initSpeedTime(final float pSpeedTime) {
+		this.mSpeedTime = pSpeedTime;
+	}
+	
+	public void initOffsetTime(final float pOffsetTime) {
+		this.mOffsetTime = pOffsetTime;
+	}
+	
+	public void initIsReverseTime(final boolean pIsReverseTime) {
+		this.mIsReverseTime = pIsReverseTime;
+	}
+	
+	public void Add(final byte x, final byte y) {
+		if(this.mListCount < mListCapacity) {
+			this.mListBX[this.mListCount] = x;
+			this.mListBY[this.mListCount] = y;
+			this.mListCount++;
+		}
+	}
+	
 	public void reset()	{
 		this.mTime = this.mOffsetTime;
 		this.onManagedUpdate(0);
+	}
+	
+	private void updatePosition() {
+		if(this.mListCount > 0) {
+			for(int i = 0; i< this.mListCount; i++)	{
+				mListX[i] = this.mListBX[i];
+				mListY[i] = this.mListBY[i];
+			}
+			for (int j = 1; j < this.mListCount; j++) {
+				int count = this.mListCount - j;
+				for (int i = 0; i < count; i++) {
+					mListX[i] = mListX[i] + (mListX[i + 1] - mListX[i]) * this.mTime;
+					mListY[i] = mListY[i] + (mListY[i + 1] - mListY[i]) * this.mTime;
+				}
+			}
+			this.setCenterPosition(this.mWidth / 2 + mListX[0] / 100 * (Options.screenWidth - this.mWidth), this.mHeight / 2 + mListY[0] / 100 * (Options.screenHeight - Options.touchHeight - Options.menuHeight - this.mHeight)); // TODO: Correct using scaled size.
+		}	
 	}
 
 	// ===========================================================
@@ -89,8 +126,6 @@ public class EntityBezier extends Entity {
 
 	@Override
 	public Entity create() {
-		this.mX_ = 0; // Last (or old) x.
-
 		this.mTime = 0f; // Seconds.
 
 		this.mMinTime = 0; // Seconds.
@@ -139,6 +174,6 @@ public class EntityBezier extends Entity {
 			}
 		}
 
-		this.updatePoints();
+		this.updatePosition();
 	}
 }
